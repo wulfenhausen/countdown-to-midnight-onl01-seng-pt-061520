8f0fb00f3ceb7de335496082a1328613a4866377
William Han Findiesen <william.findiesen@gmail.com>
2020-06-25 04:43:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,15 @@
 
 def countdown(x)
   while x > 0 
-  puts "#{x} SECOND(S)"
+  puts "#{x} SECOND(S)!"
   x -= 1 
   end
   puts "HAPPY NEW YEAR!"
 end
 
 countdown(10)
+
+def countdown_with_sleep(x)
+  while x > 0 
+  sleep 1
+  puts "#{x} SECONDS(S)!"
