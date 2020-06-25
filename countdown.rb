9bc3086e36dda3b173b6c78870e8383b1db085e2
William Han Findiesen <william.findiesen@gmail.com>
2020-06-25 04:45:55 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  while x > 0 
+  puts "#{x} SECOND(S)!"
+  x -= 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
+
+def countdown_with_sleep(x)
+  while x > 0 
+  sleep 1
+  puts "#{x} SECONDS(S)!"
+  x -= 1 
+end
+return "HAPPY NEW YEAR!"
 end
